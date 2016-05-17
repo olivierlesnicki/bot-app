fb10cfd33e782b960bdff851625fe311c5d63648
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2016-05-17 16:49:43 +0100

    first commit

---
+++ app/components/chat-box/component.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+import config from '../../config/environment';
+
+export default Ember.Component.extend({
+  messages: [],
+  text: '',
+  didInsertElement() {
+    this._socket = window.io(config.SOCKET_URL);
+    this._socket.on('reset', () => {
+      this.set('messages', []);
+    });
+    this._socket.on('message', (message) => {
+      this.get('messages').pushObject({
+        text: message.text,
+        isBot: message.emitter.indexOf(this._socket.id) === -1
+      });
+    });
+  },
+  actions: {
+    submit() {
+      this._socket.emit('message', {
+        text: this.get('text')
+      });
+      this.set('text', '');
+    }
+  }
+});
